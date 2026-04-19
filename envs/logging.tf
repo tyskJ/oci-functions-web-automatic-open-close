838e88ce09470f37eee57e15c44f3f061cc0ea97
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-19 14:57:56 +0900

    :recycle: Functions Application log 作成

--- envs/logging.tf
+++ envs/logging.tf
@@ -8,6 +8,13 @@ resource "oci_logging_log_group" "waf" {
   description    = "For Regional WAF Log Group"
 }
 
+### Functions application
+resource "oci_logging_log_group" "functions_application" {
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "functions-application-log-group"
+  description    = "For Functions Application Log Group"
+}
+
 /************************************************************
 Logs
 ************************************************************/
@@ -27,5 +34,26 @@ resource "oci_logging_log" "waf_flb" {
     }
   }
   log_group_id       = oci_logging_log_group.waf.id
+  retention_duration = 30
+}
+
+### Functions application
+resource "oci_logging_log" "functions_application" {
+  for_each = local.apps
+
+  display_name = each.value.name
+  is_enabled   = true
+  log_type     = "SERVICE"
+  configuration {
+    compartment_id = oci_identity_compartment.workload.id
+    source {
+      source_type = "OCISERVICE"
+      service     = "functions"
+      category    = "invoke"
+      resource    = oci_functions_application.this[each.key].id
+      parameters  = {}
+    }
+  }
+  log_group_id       = oci_logging_log_group.functions_application.id
   retention_duration = 30
 }
