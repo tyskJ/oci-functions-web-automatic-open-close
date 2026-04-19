237e8f5d38cdafa9233fe742e5a6111103ede3fa
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-19 11:15:24 +0900

    :recycle: 変数追加

---
+++ envs/variables.tf
@@ -0,0 +1,63 @@
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "source_ip" {
+  type = string
+}
+
+variable "subscription_email" {
+  type = string
+}
+
+variable "work_user_ocid" {
+  type = string
+}
+
+variable "repo_waf_open_prefix" {
+  type = string
+}
+
+variable "repo_waf_close_prefix" {
+  type = string
+}
+
+variable "repo_com_start_prefix" {
+  type = string
+}
+
+variable "repo_com_stop_prefix" {
+  type = string
+}
+
+variable "fn_open_name" {
+  type = string
+}
+
+variable "fn_close_name" {
+  type = string
+}
+
+variable "fn_start_name" {
+  type = string
+}
+
+variable "fn_stop_name" {
+  type = string
+}
+
+variable "fn_open_ocid" {
+  type = string
+}
+
+variable "fn_close_ocid" {
+  type = string
+}
+
+variable "fn_start_ocid" {
+  type = string
+}
+
+variable "fn_stop_ocid" {
+  type = string
+}
