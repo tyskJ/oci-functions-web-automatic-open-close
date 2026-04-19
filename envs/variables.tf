e0180934fde04edab9146b2ab0b83b955a29b19d
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-19 13:05:52 +0900

    :memo: local変数管理とする

--- envs/variables.tf
+++ envs/variables.tf
@@ -14,38 +14,6 @@ variable "work_user_ocid" {
   type = string
 }
 
-variable "repo_waf_open_prefix" {
-  type = string
-}
-
-variable "repo_waf_close_prefix" {
-  type = string
-}
-
-variable "repo_com_start_prefix" {
-  type = string
-}
-
-variable "repo_com_stop_prefix" {
-  type = string
-}
-
-variable "fn_open_name" {
-  type = string
-}
-
-variable "fn_close_name" {
-  type = string
-}
-
-variable "fn_start_name" {
-  type = string
-}
-
-variable "fn_stop_name" {
-  type = string
-}
-
 variable "fn_open_ocid" {
   type = string
 }
