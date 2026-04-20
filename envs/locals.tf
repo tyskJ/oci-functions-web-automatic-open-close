f44bf732e3e308ceeaf6e887d8a95e61274665d7
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-20 11:50:31 +0900

    :recycle: Scheduler追加

--- envs/locals.tf
+++ envs/locals.tf
@@ -80,3 +80,44 @@ locals {
     }
   }
 }
+
+/************************************************************
+Schedulers
+************************************************************/
+### 実行時間の翌日0時(JST)のUTCを取得
+resource "time_static" "base" {}
+
+locals {
+  now_jst_hour = tonumber(formatdate("H", timeadd(time_static.base.rfc3339, "9h")))
+  now_jst_min  = tonumber(formatdate("m", timeadd(time_static.base.rfc3339, "9h")))
+  now_jst_sec  = tonumber(formatdate("s", timeadd(time_static.base.rfc3339, "9h")))
+  remain_seconds = (
+    (23 - local.now_jst_hour) * 3600 +
+    (59 - local.now_jst_min) * 60 +
+    (60 - local.now_jst_sec)
+  )
+
+  waf_close_offset       = -1
+  next_jst_waf_close_utc = timeadd(time_static.base.rfc3339, "${local.remain_seconds + local.waf_close_offset * 3600}s")
+  com_start_offset       = 6
+  next_jst_com_start_utc = timeadd(time_static.base.rfc3339, "${local.remain_seconds + local.com_start_offset * 3600}s")
+}
+
+locals {
+  schedulers = {
+    waf_close = {
+      name        = "web-application-closing"
+      description = "Start WAF Closing Functions"
+      action      = "START_RESOURCE"
+      fn_ocid     = var.fn_close_ocid
+      time_start  = local.next_jst_waf_close_utc
+    }
+    com_start = {
+      name        = "web-application-opening"
+      description = "Start Compute Start Functions"
+      action      = "START_RESOURCE"
+      fn_ocid     = var.fn_start_ocid
+      time_start  = local.next_jst_com_start_utc
+    }
+  }
+}
--- envs/resource_scheduler.tf
+++ envs/resource_scheduler.tf
@@ -3,18 +3,17 @@ Schedules
 ************************************************************/
 ### For Functions
 resource "oci_resource_scheduler_schedule" "functions" {
-  count = var.fn_schedules ? 1 : 0
+  for_each = var.fn_schedules ? local.schedulers : {}
 
   compartment_id = oci_identity_compartment.workload.id
   state          = "ACTIVE"
-  display_name   = "web-application-closing"
-  description    = "Start WAF Closing Functions"
-  action         = "START_RESOURCE"
+  display_name   = each.value.name
+  description    = each.value.description
+  action         = each.value.action
   resources {
-    id = var.fn_close_ocid
+    id = each.value.fn_ocid
   }
   recurrence_details = "FREQ=DAILY;INTERVAL=1"
   recurrence_type    = "ICAL"
+  time_starts        = each.value.time_start
 }
-
-### For Compute 
